23d2824370fb5b2e095ea1a407b459b0d19a139f
Marcelo Neris <marceloaneris@gmail.com>
2022-11-11 19:20:15 -0300

    segundo commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         e.preventDefault();
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
         const novoItem = $('<li style="display: none"></li>');
-        $(`<img scr="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
             <div class="imagem-link">
                 <a href="${enderecoDaNovaImagem}" target='_blank' title="Ver imagem em tamanho real">
